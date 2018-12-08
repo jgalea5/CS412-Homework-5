10a4ddd2d1a4f309cb422e579d281579e5235d48
Jose <33208136+jgalea5@users.noreply.github.com>
2018-12-07 20:37:14 -0600

    Updated testing.py

--- testing.py
+++ testing.py
@@ -6,14 +6,36 @@ from numpy import *
 def main():
 	Xte, Yte = feature_label_split("data/responses.te")
 	print('Getting test data...')
-	print('retrieving trained random forest model...')
-	pickle_file = open('rand_forest_classifier.pkl', 'rb')
-	classifier = pickle.load(pickle_file)
-	s1 = classifier.predict(Xte)
-	pickle_file.close()
-	print('predicting test sample data...')
+	print()
+	pickle_file = open('models.pkl', 'rb')
+	classifiers = pickle.load(pickle_file)
 
+	print('predicting test sample data with mostfrequent.')
+	mostfrequent = classifiers[0]
+	s1 = mostfrequent.predict(Xte)
 	print("the accuracy on the testing data is :{0}".format(mean(s1==Yte)))
+	print()
+	print('predicting test sample data with forest.')
+	forest = classifiers[1]
+	s2 = forest.predict(Xte)
+	print("the accuracy on the testing data is :{0}".format(mean(s2==Yte)))
+	print()
+	print('predicting test sample data with decision tree.')
+	tree = classifiers[2]
+	s3 = tree.predict(Xte)
+	print("the accuracy on the testing data is :{0}".format(mean(s3==Yte)))
+	print()
+	print('predicting test sample data with knn.')
+	knn = classifiers[3]
+	s4 = knn.predict(Xte)
+	print("the accuracy on the testing data is :{0}".format(mean(s4==Yte)))
+	print()
+	print('predicting test sample data with svc.')
+	svc = classifiers[4]
+	s5 = svc.predict(Xte)
+	print("the accuracy on the testing data is :{0}".format(mean(s5==Yte)))
+	print()
+	pickle_file.close()
 
 if __name__ == '__main__':
 	main()
